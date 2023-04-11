ad0cc0adfe9a2f89c921357d39f30a8011f539b8
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-04-11 19:57:30 +0100

    Resolução de bug referente ao controller de nota de entrada

--- SisControle/src/main/java/com/SisControle/controller/NotaEntradaController.java
+++ SisControle/src/main/java/com/SisControle/controller/NotaEntradaController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,10 +16,12 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.SisControle.bo.FornecedorBO;
 import com.SisControle.bo.NotaEntradaBO;
+import com.SisControle.bo.ProdutoBO;
 import com.SisControle.model.NotaEntrada;
+import com.SisControle.model.NotaEntradaItem;
 
 @Controller
-@RequestMapping("/nota-entrada")
+@RequestMapping("/notas-entrada")
 public class NotaEntradaController {
 
 	@Autowired
@@ -27,18 +30,22 @@ public class NotaEntradaController {
 	@Autowired
 	private FornecedorBO fornecedorBO;
 
+	@Autowired
+	ProdutoBO produtoBO;
+
 	@GetMapping("/novo")
 	public ModelAndView novo(ModelMap model) {
 		model.addAttribute("notaEntrada", new NotaEntrada());
 		model.addAttribute("fornecedores", fornecedorBO.findAll());
-		return new ModelAndView("/nota-entrada/formulario", model);
+		return new ModelAndView("/notas-entrada/formulario", model);
 	}
 
 	@PostMapping("")
-	public String salva(@Valid @ModelAttribute NotaEntrada notaEntrada, BindingResult result, RedirectAttributes attr, ModelMap model) {
+	public String salva(@Valid @ModelAttribute NotaEntrada notaEntrada, BindingResult result, RedirectAttributes attr,
+			ModelMap model) {
 
 		if (result.hasErrors()) {
-			return "/nota-entrada/formulario";
+			return "/notas-entrada/formulario";
 		}
 		if (notaEntrada.getId() == null) {
 			notaEntradaBO.insert(notaEntrada);
@@ -47,13 +54,38 @@ public class NotaEntradaController {
 			notaEntradaBO.update(notaEntrada);
 			attr.addFlashAttribute("feedback", "Os dados da nota foram atualizados com sucesso");
 		}
-		return "redirect:/nota-entrada";
+		return "redirect:/notas-entrada";
 	}
 
-	@GetMapping("/lista")
+	@GetMapping("")
 	public ModelAndView lista(ModelMap model) {
 		model.addAttribute("notas", notaEntradaBO.findAll());
-		return new ModelAndView("/nota-entrada/lista", model);
+		return new ModelAndView("/notas-entrada/lista", model);
 	}
 
+	@GetMapping("/{id}/item")
+	public ModelAndView produto(@PathVariable("id") Long id, ModelMap model) {
+		NotaEntradaItem nei = new NotaEntradaItem();
+		NotaEntrada ne = notaEntradaBO.findById(id);
+		nei.setNotaEntrada(ne);
+		model.addAttribute("notaEntradaItem", nei);
+		model.addAttribute("produtos", produtoBO.findAll());
+		return new ModelAndView("/notas-entrada-item/formulario", model);
+	}
+
+	@GetMapping("/edita/{id}")
+	public ModelAndView edita(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("notaEntradaItem", new NotaEntradaItem());
+		model.addAttribute("fornecedores", fornecedorBO.findAll());
+		model.addAttribute("notaEntrada", notaEntradaBO.findById(id));
+		return new ModelAndView("/notas-entrada/formulario", model);
+	}
+
+	@GetMapping("/remove/{id}")
+	public String remove(@PathVariable("id") Long id, RedirectAttributes attr) {
+		NotaEntrada ne = notaEntradaBO.findById(id);
+		notaEntradaBO.remove(ne);
+		attr.addFlashAttribute("feedback", "Nota entrada removida com sucesso");
+		return "redirect:/notas-entrada";
+	}
 }
